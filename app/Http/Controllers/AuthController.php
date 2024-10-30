5d5180ff5f056320ead44ffce59e9ee3f82b6b4b
Mohsin Khan <mkhan9658@gmail.com>
2024-10-30 23:05:33 +0500

    migration and code updates

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,36 +19,50 @@ class AuthController extends Controller
     {
         try {
             $keycloakUser = Socialite::driver('keycloak')->user();
-    
-            $existingUser = User::where('email', $keycloakUser->getEmail())->first();
-    
+
+            $existingUser = User::where('keycloak_id', $keycloakUser->getId())->first();
+
             if ($existingUser) {
                 Auth::login($existingUser);
             } else {
-                $newUser = User::create([
-                    'name' => $keycloakUser->getName(),
-                    'email' => $keycloakUser->getEmail(),
-                    'password' => bcrypt('newporjetc1password'), // Assign a default password
-                ]);
-    
-                Auth::login($newUser);
+                $existingUserByEmail = User::where('email', $keycloakUser->getEmail())->first();
+
+                if ($existingUserByEmail) {
+                    $existingUserByEmail->update([
+                        'keycloak_id' => $keycloakUser->getId(),
+                    ]);
+                    Auth::login($existingUserByEmail);
+                } else {
+                    $newUser = User::create([
+                        'name' => $keycloakUser->getName(),
+                        'email' => $keycloakUser->getEmail(),
+                        'keycloak_id' => $keycloakUser->getId(),
+                        'password' => bcrypt('newprojectpassword'),
+                    ]);
+                    Auth::login($newUser);
+                }
             }
-    
+
             return redirect()->intended('/');
         } catch (\Exception $e) {
-
             return redirect('/login')->withErrors(['msg' => 'Unable to log in. Please try again.']);
         }
     }
 
-public function logout()
-{
-    Auth::logout();
-
-    session()->invalidate();  // Invalidate the session
-    session()->regenerateToken();  // Prevent CSRF attacks
-
-    return redirect('/login');  // Redirect to login page
-}
+    public function logout()
+    {
+        Auth::logout();
+    
+        session()->invalidate();
+        session()->regenerateToken();
+    
+        $redirectUri = url('/login');
+        $clientId = config('services.keycloak.client_id');
+        dd($clientId);
+        $keycloakLogoutUrl = Socialite::driver('keycloak')->getLogoutUrl($redirectUri, $clientId);
+    
+        // Redirect the user to the Keycloak logout URL
+        return redirect($keycloakLogoutUrl);
+    }
 
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,9 @@
-<form method="POST" action="{{ route('logout') }}">
-    @csrf
-    <button type="submit">Logout</button>
-</form>
+@if(Auth::check())
+    <p>Welcome, {{ Auth::user()->name }}!</p>
+    <form method="POST" action="{{ route('logout') }}">
+        @csrf
+        <button type="submit">Logout</button>
+    </form>
+@else
+    <a href="{{ route('login') }}">Login with Keycloak</a>
+@endif
